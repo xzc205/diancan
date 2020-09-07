4885012cf6f32cbc173d2d2b9d4d38047f1859db
熊志成 <70554730+xzc205@users.noreply.github.com>
2020-09-07 08:56:49 +0800

    Create asinhf.c
    熊志成

---
+++ asinhf.c
@@ -0,0 +1,9 @@
+/* asinhf function */
+/* Copyright 2009-2010 IAR Systems AB. */
+#include "xxfftype.h"
+#include "xxasinh.h"
+
+/*
+ * Copyright (c) 1992-2009 by P.J. Plauger.  ALL RIGHTS RESERVED.
+ * Consult your license regarding permissions and restrictions.
+V5.04:0576 */
